971f4487c1301b46f992676da71d202c862b4a08
rahmaw00 <ramata.will@gmail.com>
2016-04-13 20:26:20 +0000

    adding my work

--- starter_code/js/wall.js
+++ starter_code/js/wall.js
@@ -2,5 +2,12 @@ $(document).ready(function()
 {
 	$('#message').focus();
 
-	// YOUR CODE GOES HERE
+	$("button").click(function() {
+	var postToWall = $("#message").val();
+	$("#wall").prepend("<div><img src= 'http://i.telegraph.co.uk/multimedia/archive/03481/Grumpy_Cat__3481823b.jpg>' width='100'/>"+postToWall+"</div>" );
+	
+//	$('input').attr('#message');
+//	$('body').append('#message');
+	});
+	
 });
